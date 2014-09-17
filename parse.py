751e18f88947d7b5c71d0f4f9545727282648535
Deja Vu <dejavu@ubuntu.(none)>
2014-09-17 03:39:36 -0700

    any

--- parse.py
+++ parse.py
@@ -1,8 +1,9 @@
 import subprocess
+import time
 #rom In
 #output = subprocess.call('./input.sh', shell=True)
 
-Config_Loss_Small_Interval = 2 # collect data every second
+Config_Loss_Small_Interval = 1 # collect data every second
 HorizonLength = 1
 
 class Packet:
@@ -44,11 +45,11 @@ def MergePcap1(pcap_out, pcap_in, ip_out, ip_in):
     unmatched_out, unmatched_in = {}, {}
     stop_in = False
     stop_out = False
-    out_lead_in = 0
     start_time_out = None
-    last_out_time, last_in_time = None, None
-    max_time_diff, min_time_diff = 0, 0
     packet_count_in_small_interval = 0
+    synched = False
+    out_lead_in = 0
+    in_lead_out = 0
     while ((not stop_in) or (not stop_out)):
         packet_match, packet_out, packet_in = None, None, None
         if not stop_out and out_lead_in >= 0:
@@ -68,18 +69,23 @@ def MergePcap1(pcap_out, pcap_in, ip_out, ip_in):
         else:
             out_key_in_in_dict, in_key_in_out_dict = False, False
             if packet_out:
-                last_out_time = packet_out.time
                 if packet_out.key in unmatched_in.keys():
                     if abs(unmatched_in[packet_out.key].time - packet_out.time) < HorizonLength:
                         out_key_in_in_dict = True
                 if out_key_in_in_dict:
                     packet_match = packet_out.OutMergeIn(unmatched_in[packet_out.key])
                     unmatched_in.pop(packet_out.key, None)
+                    # unless the in dict is obviously too large to query
+                    
+                        in_lead_out = 3 - len(unmatched_in)
+                    if synched == False:
+                        print 'synched'
+                        time.sleep(5)
+                    synched = True
                 else:
                     unmatched_out[packet_out.key] = packet_out
                     pass
             if packet_in:
-                last_in_time = packet_in.time
                 if packet_in.key in unmatched_out.keys():
                     if abs(packet_in.time - unmatched_out[packet_in.key].time) < HorizonLength:
                         in_key_in_out_dict = True
@@ -91,25 +97,22 @@ def MergePcap1(pcap_out, pcap_in, ip_out, ip_in):
                 else:
                     unmatched_in[packet_in.key] = packet_in
                     pass
+        print len(unmatched_out), len(unmatched_in)
 
         if packet_match:
             yield {'key':packet_match.key, 'delay':packet_match.time_in - packet_match.time_out, 'out':packet_match.time_out}
-        if packet_out and out_lead_in >= 0:
+        if packet_out and synched == True:
             if start_time_out == None or packet_out.time - start_time_out > Config_Loss_Small_Interval:
                 # clear un found keys
+#                print 'packet out', packet_out.key, packet_out.time
                 for (key, packet) in unmatched_out.items():
                     if packet_out.time - packet.time > 0:
-                        print key, packet.time, packet_out.key, packet_out.time
+#                        print key, packet.time
                         unmatched_out.pop(key, None)
                 if start_time_out != None:
                     # summary un received packet in out
                     un_received_packet_in_out = len(unmatched_out)
-                    if out_lead_in >= 0:
-#                        print un_received_packet_in_out, packet_count_in_small_interval
-                        if packet_count_in_small_interval > 0:
-                            yield {'loss':1-float(un_received_packet_in_out)/float(packet_count_in_small_interval), 'out':packet_out.time}
-
+                    if packet_count_in_small_interval > 0:
+                        yield {'loss':1-float(un_received_packet_in_out)/float(packet_count_in_small_interval), 'out':packet_out.time}
                 start_time_out = packet_out.time
                 packet_count_in_small_interval = 1
-            else:
-                packet_count_in_small_interval = packet_count_in_small_interval + 1
