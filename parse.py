bfd3592f281cb12abab444045e6ab25fe1232574
Deja Vu <dejavu@ubuntu.(none)>
2014-09-16 23:45:44 -0700

    parse

--- parse.py
+++ parse.py
@@ -26,7 +26,7 @@ def ParsePcap(pcap, ip1, ip2):
             vals = line.split(';')
             vals = [val.rstrip('\n').lstrip('"').rstrip('"') for val in vals]
             packet = Packet()
-            ipid = int(vals[1], 16) if vals[1].startswith('0x') else int(vals[1], 10)
+            ipid = vals[1]
             prot = vals[2]
             cap_len = int(vals[3])
             packet.key = (ipid, prot)#, cap_len)
@@ -43,46 +43,49 @@ def MergePcap1(pcap_out, pcap_in, ip_out, ip_in):
     unmatched_out, unmatched_in = {}, {}
     stop_in = False
     stop_out = False
+    out_lead_in = 0
     while ((not stop_in) or (not stop_out)):
         packet_match, packet_out, packet_in = None, None, None
-        if not stop_out:
+        if not stop_out and out_lead_in <= 0:
             try:
                 packet_out = Out.next()
             except StopIteration:
                 stop_out = True
-        if (not stop_in):
+        if (not stop_in) and out_lead_in >= 0:
             try:
                 packet_in = In.next()
             except StopIteration:
                 stop_in = True
         if packet_out and packet_in and packet_out.key == packet_in.key:
             packet_match = packet_out.OutMergeIn(packet_in)
+            out_lead_in = 0
         else:
             out_key_in_in_dict, in_key_in_out_dict = False, False
             if packet_out:
                 if packet_out.key in unmatched_in.keys():
-                    if unmatched_in[packet_out.key].time - packet_out.time < HorizonLength:
+                    if abs(unmatched_in[packet_out.key].time - packet_out.time) < HorizonLength:
                         out_key_in_in_dict = True
                 if out_key_in_in_dict:
                     packet_match = packet_out.OutMergeIn(unmatched_in[packet_out.key])
                     unmatched_in.pop(packet_out.key, None)
-                    for key in unmatched_out.keys():
-                        if packet_match.time_out - unmatched_out[key].time > HorizonLength:
-                            unmatched_out.pop(key, None)
-                    pass
+                    #for key in unmatched_out.keys():
+                    #    if packet_match.time_out - unmatched_out[key].time > HorizonLength:
+                    #        unmatched_out.pop(key, None)
+                    out_lead_in = -1
                 else:
                     unmatched_out[packet_out.key] = packet_out
                     pass
             if packet_in:
                 if packet_in.key in unmatched_out.keys():
-                    if packet_in.time - unmatched_out[packet_in.key].time < HorizonLength:
+                    if abs(packet_in.time - unmatched_out[packet_in.key].time) < HorizonLength:
                         in_key_in_out_dict = True
                 if in_key_in_out_dict:
                     packet_match = packet_in.InMergeOut(unmatched_out[packet_in.key])
                     unmatched_out.pop(packet_in.key, None)
-                    for key in unmatched_in.keys():
-                        if packet_match.time_in - unmatched_in[key].time > HorizonLength:
-                            unmatched_in.pop(key, None)
+                    #for key in unmatched_in.keys():
+                    #    if packet_match.time_in - unmatched_in[key].time > HorizonLength:
+                    #        unmatched_in.pop(key, None)
+                    out_lead_in = 1
                     pass
                 else:
                     unmatched_in[packet_in.key] = packet_in
@@ -90,3 +93,5 @@ def MergePcap1(pcap_out, pcap_in, ip_out, ip_in):
 
         if packet_match:
             yield {'key':packet_match.key, 'delay':packet_match.time_in - packet_match.time_out, 'out':packet_match.time_out}
+
+       # print out_lead_in, len(unmatched_out), len(unmatched_in)
